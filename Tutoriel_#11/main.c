6e4d5af73857fdb7f789a229200b4a697a53239b
BeScriptDev <bescript.dev@gmail.com>
2023-03-31 15:31:27 +0200

    Tutoriel_#11

--- Tutoriel_#11/main.c
+++ Tutoriel_#11/main.c
@@ -2,19 +2,26 @@
 
 int main(void) {
     user_t user[3];
-    user[0].firstname = bs_strdup("John");
+
+    user[0].firstname = bs_strdup("Alex");
     user[0].lastname = bs_strdup("Doe");
-    user[0].age = 50;
+    user[0].age = 30;
 
-    user[1].firstname = bs_strdup("Alex");
+    user[1].firstname = bs_strdup("John");
     user[1].lastname = bs_strdup("Doe");
-    user[1].age = 20;
+    user[1].age = 34;
 
-    user[2].firstname = bs_strdup("Jean");
+    user[2].firstname = bs_strdup("Mick");
     user[2].lastname = bs_strdup("Doe");
-    user[2].age = 40;
+    user[2].age = 65;
+
     for (int i = 0; i < 3; i++) {
         printf("Firstname -> %s, Lastname -> %s, Age -> %d\n", user[i].firstname, user[i].lastname, user[i].age);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        free(user[i].firstname);
+        free(user[i].lastname);
     }
     return 0;
 }
